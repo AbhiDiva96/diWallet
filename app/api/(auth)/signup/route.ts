70a84fc7968942e92b1620518c278a889cff95b9
ABHISHEK KUMAR <abhishekblog6315@gmail.com>
2024-10-14 18:58:04 +0530

    errorhandler

--- app/api/(auth)/signup/route.ts
+++ app/api/(auth)/signup/route.ts
@@ -1,39 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
-import bcrypt   from 'bcrypt';
+import bcrypt from 'bcrypt';
 import { prisma } from "@/lib/prisma";
 
+export async function POST(req: NextRequest) {
+    // Parse the request body
+    const { name, email, password } = await req.json();
 
-export async function POST(req: NextRequest){
-
-    const {name , email, password} = await req.json();
-
-    if(!name || !email || !password){
-        return NextResponse.json({message:'fields are required'}, {status:400})
+    // Check if all fields are provided
+    if (!name || !email || !password) {
+        return NextResponse.json({ message: 'Fields are required' }, { status: 400 });
     }
-   
+
+    // Check if the user already exists
     const existingUser = await prisma.user.findUnique({
-        where: {email}
-    })
+        where: { email }
+    });
 
-    if(existingUser){
-        return NextResponse.json({message:'user already exists'}, {status:401})
+    if (existingUser) {
+        return NextResponse.json({ message: 'User already exists' }, { status: 401 });
     }
 
-    const hashedPassword = await bcrypt.hash(password,10);
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    try{
-         
-      const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-            passwordHash: hashedPassword,       
-        }
-      })
+    try {
+        // Create a new user
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                passwordHash: hashedPassword,
+            }
+        });
 
-        return NextResponse.json({message:'user created successfully', user}, {status:200})
-    }catch(error){
-        console.error();
-        return NextResponse.json({message:'geting error while submiting', error}, {status:500})
+        return NextResponse.json({ message: 'User created successfully', user }, { status: 201 });
+    } catch (error) {
+        console.error('Error creating user:', error);
+        return NextResponse.json({ message: 'Error while creating user', error }, { status: 500 });
     }
 }
